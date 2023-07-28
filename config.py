dfa21beb58e2ef8420877b97bdd2d0bf264d2569
vt-bekah <vt_bekah@hotmail.com>
2023-07-28 08:12:40 -0500

    added markdown cell at the top that described approach and analysis summary

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 bls_key = "5278c41dd2d8414399a7542c03c1e8ac"
-
 weather_key = "5611c6f55d411edb9a5681ba2717e0cc"
 geoapify_key = "fb9ddf60e81e4758b506495b0201e6ee"
 census_key = "d8c8ff7903d1e4880de7ca5ae0dcc565ea162158"
+attom_key = "1c7894223ed1e5843062382304b4fbd2"
